97cbb78d9f641e1c30e7758f4fbe5c476dfe0ba6
Max-APL <maxpasten15908@gmail.com>
2025-02-27 18:59:00 -0400

    Estructura de otros microservicios

---
+++ ms-report/src/main/java/com/hazmelaucb/ms_report/MsReportApplication.java
@@ -0,0 +1,13 @@
+package com.hazmelaucb.ms_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReportApplication.class, args);
+	}
+
+}
