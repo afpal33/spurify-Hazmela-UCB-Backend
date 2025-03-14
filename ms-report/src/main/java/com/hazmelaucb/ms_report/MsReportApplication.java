1361fdb4a53c378bd0c7582144e5c1ff59253f09
MarcoYeahBuddy <ticona465@gmail.com>
2025-03-14 13:40:08 -0400

    2DO BASE

--- ms-report/src/main/java/com/hazmelaucb/ms_report/MsReportApplication.java
+++ ms-report/src/main/java/com/hazmelaucb/ms_report/MsReportApplication.java
@@ -3,6 +3,17 @@ package com.hazmelaucb.ms_report;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication(scanBasePackages = "com.hazmelaucb.ms_report.config")
+@ComponentScan(basePackages = {
+    "com.hazmelaucb.ms_report.config",
+    "com.hazmelaucb.ms_report.controller",
+    "com.hazmelaucb.ms_report.config",
+    "com.hazmelaucb.ms_report.config",
+    "com.hazmelaucb.ms_report.config",
+    "com.hazmelaucb.ms_report.config"  
+})
+
 @SpringBootApplication
 public class MsReportApplication {
 
