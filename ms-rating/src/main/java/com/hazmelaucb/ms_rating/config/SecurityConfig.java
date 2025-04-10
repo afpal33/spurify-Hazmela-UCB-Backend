3827c6a1fb32c275dd7fa91f99f4a5fda42b73cf
Max-APL <maxpasten15908@gmail.com>
2025-04-10 11:17:21 -0400

    Docker composer y dockerfile del ms-user

--- ms-rating/src/main/java/com/hazmelaucb/ms_rating/config/SecurityConfig.java
+++ ms-rating/src/main/java/com/hazmelaucb/ms_rating/config/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.hazmelaucb.ms_rating.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 🔥 Permitir TODO sin autenticación
+                )
+                .csrf(csrf -> csrf.disable()) // Opcional: Desactiva CSRF si es necesario
+                .build();
+    }
 }
