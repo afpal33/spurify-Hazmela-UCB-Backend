dda2195fb405e986f057c9699b9692eaf7de8a63
Max-APL <maxpasten15908@gmail.com>
2025-05-22 11:13:58 -0400

    try config server

---
+++ config-server/src/main/java/com/hazmelaucb/config_server/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.hazmelaucb.config_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                // Disable CRCF to allow POST to /encrypt and /decrypt endpoins
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> {})
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
